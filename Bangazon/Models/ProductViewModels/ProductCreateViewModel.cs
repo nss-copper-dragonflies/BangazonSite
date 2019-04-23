32bf11b39092f1ed9b3064730dae60eb8af24ec8
unknown <bramosjaneway@gmail.com>
2019-04-23 15:43:43 -0500

    added error for Product Category

--- Bangazon/Models/ProductViewModels/ProductCreateViewModel.cs
+++ Bangazon/Models/ProductViewModels/ProductCreateViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Bangazon.Models.ProductViewModels
     public class ProductCreateViewModel
     {
         public Product Product { get; set; }
+
         public List<SelectListItem> ProductTypes { get; set; }
         
     }
